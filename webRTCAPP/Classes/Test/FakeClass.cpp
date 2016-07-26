4b442f74b1e781e35e892a54148c165723743b77
wangxin <raymon_wang@wangxindeMac-mini.local>
2016-07-26 17:30:20 +0800

    增加新的工程目标配置

---
+++ webRTCAPP/Classes/Test/FakeClass.cpp
@@ -0,0 +1,9 @@
+//
+//  FakeClass.cpp
+//  webRTCAPP
+//
+//  Created by wangxin on 16/7/26.
+//  Copyright © 2016年 yunwei. All rights reserved.
+//
+
+#include "FakeClass.hpp"
---
+++ webRTCAPP/Classes/Test/FakeClass.hpp
@@ -0,0 +1,15 @@
+//
+//  FakeClass.cpp
+//  webRTCAPP
+//
+//  Created by wangxin on 16/7/26.
+//  Copyright © 2016年 yunwei. All rights reserved.
+//
+
+#include "FakeClass.hpp"
+
+class FakeClass {
+public:
+    FakeClass() {};
+    virtual ~FakeClass() {};
+};
