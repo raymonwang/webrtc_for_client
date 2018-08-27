8fd01853e49879becd5c0937f51276504b6a5efb
raymon_wang <wang3140@hotmail.com>
2018-08-27 18:55:52 +0800

    fix compile problem

--- webRTCAPP/proj.android/jni/Android.mk
+++ webRTCAPP/proj.android/jni/Android.mk
@@ -176,14 +176,14 @@ endif
 #$(warning "$(LOCAL_C_INCLUDES)")			    
 
 LOCAL_CFLAGS += -DANDROID -DWEBRTC_POSIX -DWEBRTC_ANDROID -DWEBRTC_LINUX -DWEBRTC_OPUS_SUPPORT_120MS_PTIME=0 -DWEBRTC_OPUS_VARIABLE_COMPLEXITY=0
-LOCAL_CFLAGS += -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -DDYNAMIC_ANNOTATIONS_ENABLED=1 -DWTF_USE_DYNAMIC_ANNOTATIONS=1 -D__GNU_SOURCE=1 -DWEBRTC_BUILD_LIBEVENT -DGOOGLE_PROTOBUF_NO_RTTI -DGOOGLE_PROTOBUF_NO_STATIC_INITIALIZER   -DHAVE_WEBRTC_VOICE -DRTCHAT_OPUS_FREQ=16000
+LOCAL_CFLAGS += -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -DDYNAMIC_ANNOTATIONS_ENABLED=1 -DWTF_USE_DYNAMIC_ANNOTATIONS=1 -D__GNU_SOURCE=1 -DWEBRTC_BUILD_LIBEVENT -DGOOGLE_PROTOBUF_NO_RTTI -DGOOGLE_PROTOBUF_NO_STATIC_INITIALIZER   -DHAVE_WEBRTC_VOICE -DRTCHAT_OPUS_FREQ=16000 -DHAVE_PTHREAD_COND_TIMEDWAIT_MONOTONIC
 LOCAL_CFLAGS += -Wno-narrowing
 
 LOCAL_CFLAGS += -fno-strict-aliasing --param=ssp-buffer-size=4 -funwind-tables -fPIC -pipe -ffunction-sections -fno-short-enums -finline-limit=64 -Wall -Wno-uninitialized -Wno-missing-field-initializers -Wno-unused-parameter -fvisibility=hidden -Wextra -Wno-missing-field-initializers -Wno-strict-overflow -Os -fno-ident -fdata-sections -g0 -fno-builtin-cos -fno-builtin-sin -fno-builtin-cosf -fno-builtin-sinf -Wno-unused-local-typedefs -Wno-unused-function
 LOCAL_CPPFLAGS += -fno-threadsafe-statics -fvisibility-inlines-hidden -std=gnu++11 -Wno-narrowing -fno-rtti -fno-exceptions -Woverloaded-virtual -fuse-ld=gold -Wno-reorder
 
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)				    
-LOCAL_CFLAGS += -fstack-protector -march=armv7-a -mfloat-abi=softfp -mtune=generic-armv7-a  -fno-caller-saves -mfpu=neon -Wno-psabi -fomit-frame-pointer
+LOCAL_CFLAGS += -fstack-protector -march=armv7-a -mfloat-abi=softfp -mtune=generic-armv7-a  -fno-caller-saves -mfpu=neon -fomit-frame-pointer
 else ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
 LOCAL_CFLAGS += -fstack-protector  -fno-omit-frame-pointer 
 else ifeq ($(TARGET_ARCH_ABI), x86)
